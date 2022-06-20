dd1b4e9ee7e75c577db3db2cd4cd2385941ada54
Serdar Tayan <34seta@gmail.com>
2022-06-20 16:05:27 +0300

    hipotenüs hesaplama tamamlandı.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        //Üçgenin Hipotenüsünü Hesaplama
+
+        double a,b,c;
+
+        System.out.println("Lütfen üçgenin kısa kenarını 'a' giriniz= ");
+        a= inp.nextDouble();
+        System.out.println("Lütfen üçgenin uzun 'b' kenarını giriniz= ");
+        b= inp.nextDouble();
+
+        c=Math.sqrt(a*a+b*b);
+        System.out.println("Hipotenüs= "+ c);
+    }
+}
